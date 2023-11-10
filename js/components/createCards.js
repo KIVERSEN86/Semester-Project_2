9c4a6513ac29ad00fe84173f60839bc4cb115fa0
Kristoffer Iversen <73189395+KIVERSEN86@users.noreply.github.com>
2023-11-10 17:40:33 +0100

    test update

--- js/components/createCards.js
+++ js/components/createCards.js
@@ -1,7 +1,16 @@
-export function createCards() {
+export function createCards(json) {
   const container = document.querySelector(".container__weekly");
 
+  container.innerHTML = "";
+
   for (let i = 0; i < json.data.length; i++) {
-    container.innerHTML += `<div>${json.data[i].attributes.content}</div>`;
+    container.innerHTML += `
+                           <a href="article-details.html?id=${json.data[i].id}"><div class="card" style="width: 24rem;">
+                           <img src="http://localhost:1338${json.data[i].attributes.image.data.attributes.url}" class="card-img-top img-width" alt="${json.data[i].attributes.image.data.attributes.alternativeText}">
+                              <div class="card-body">
+                                <h5 class="card-title">${json.data[i].attributes.title}</h5>
+                                <a href="article-details.html?id=${json.data[i].id}" class="btn btn-cta">Read more</a>
+                              </div>
+                            </div></a>`;
   }
 }
--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ import { createNav } from "./components/createNav.js";
 import { createFooter } from "./components/createFooter.js";
 import { baseUrl } from "./components/constants/baseUrl.js";
 import { signUp } from "./components/signUp.js";
+import { createCards } from "./components/createCards.js";
 
 createNav();
 signUp();
@@ -11,25 +12,12 @@ createFooter();
 const postsUrl = baseUrl + "articles?populate=image";
 
 (async function () {
-  const container = document.querySelector(".container__weekly");
   try {
     const response = await fetch(postsUrl);
     const json = await response.json();
 
     console.log(json);
-
-    container.innerHTML = "";
-
-    for (let i = 0; i < json.data.length; i++) {
-      container.innerHTML += `
-                           <a href="article-details.html?id=${json.data[i].id}"><div class="card" style="width: 24rem;">
-                           <img src="http://localhost:1338${json.data[i].attributes.image.data.attributes.url}" class="card-img-top img-width" alt="${json.data[i].attributes.image.data.attributes.alternativeText}">
-                              <div class="card-body">
-                                <h5 class="card-title">${json.data[i].attributes.title}</h5>
-                                <a href="article-details.html?id=${json.data[i].id}" class="btn btn-cta">Read more</a>
-                              </div>
-                            </div></a>`;
-    }
+    createCards(json);
   } catch (error) {
     showMessage("warning", "Ops! There is a problem with retrieving data from the API", ".message-container");
   }
